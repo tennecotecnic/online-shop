68427373e0a9c6b794a749f678f99876f691bcdc
EuhenyP <tennecotecnic@gmail.com>
2024-02-18 19:05:40 +0300

    TOS-5. Добавлена сущность юзера, геттеры и сеттеры.

---
+++ src/main/java/com/tennecotecnic/onlineshop/model/Sex.java
@@ -0,0 +1,6 @@
+package com.tennecotecnic.onlineshop.model;
+
+public enum Sex {
+    MALE,
+    FEMALE
+}
--- src/main/java/com/tennecotecnic/onlineshop/model/User.java
+++ src/main/java/com/tennecotecnic/onlineshop/model/User.java
@@ -1,12 +1,16 @@
 package com.tennecotecnic.onlineshop.model;
-
+import java.time.Instant;
 public class User {
 
     private Integer id;
-
-    public String name;
-
-
+    private String name;
+    private String surName;
+    private String e_mail;
+    private Integer birthYear;
+    private Sex sex;
+    private Integer totalPurchasesCount;
+    private Integer averagePurchasesPerDay;
+    private Instant createdAt;
     public User(String name) {
         this.name = name;
     }
@@ -33,11 +37,58 @@ public class User {
 
 
     public User() {
-
     }
-
 
     public User(Integer id) {
         this.id = id;
     }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getE_mail() {
+        return e_mail;
+    }
+
+    public void setE_mail(String e_mail) {
+        this.e_mail = e_mail;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Integer getTotalPurchasesCount() {
+        return totalPurchasesCount;
+    }
+
+    public void setTotalPurchasesCount(Integer totalPurchasesCount) {
+        this.totalPurchasesCount = totalPurchasesCount;
+    }
+
+    public Integer getAveragePurchasesPerDay() {
+        return averagePurchasesPerDay;
+    }
+
+    public void setAveragePurchasesPerDay(Integer averagePurchasesPerDay) {
+        this.averagePurchasesPerDay = averagePurchasesPerDay;
+    }
+
 }
